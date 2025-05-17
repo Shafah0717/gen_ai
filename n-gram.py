b5c67b51e887b887005b486890f6deeda5e842b8
Shafah0717 <shafahmuhammed@gmail.com>
2025-05-17 23:13:20 +0530

    n-gram

---
+++ n-gram.py
@@ -0,0 +1,15 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.util import ngrams
+
+sentence = "I am learning ngrams"
+tokens = word_tokenize(sentence)
+
+unigram = list(ngrams(tokens,1))
+print("unigram:",unigram)
+
+bigram = list(ngrams(tokens,2))
+print("bigram:",bigram)
+
+trigram = list(ngrams(tokens,3))
+print("trigram:",trigram)
